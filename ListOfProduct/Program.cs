4f9640acd28728bd9215189931f1a9d9a9412739
MetodyAleksov <71280200+MetodyAleksov@users.noreply.github.com>
2020-10-14 18:49:20 +0300

    Updated

---
+++ ListOfProduct/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ListOfProduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> listOfProducts = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                listOfProducts.Add(input);
+            }
+            for (int i = 0; i < listOfProducts.Count; i++)
+            {
+                Console.WriteLine($"{i+1}. {listOfProducts[i]}");
+            }
+        }
+    }
+}
