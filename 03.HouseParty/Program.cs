f48e1c53e5bb0b492a4f406b604d463c5356d782
MetodyAleksov <71280200+MetodyAleksov@users.noreply.github.com>
2020-10-16 17:37:43 +0300

    updated

--- 03.HouseParty/Program.cs
+++ 03.HouseParty/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _03.HouseParty
 {
@@ -6,7 +8,43 @@ namespace _03.HouseParty
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = new List<string>(n);
+            for (int i = 0; i < n; i++)
+            {
+                List<string> input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+                string name = input[0];
+                input.RemoveAt(0);
+                string centence = string.Join(" ", input);
+                if (centence == "is not going!")
+                {
+                    if (names.Contains(name))
+                    {
+                        names.Remove(name);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{name} is not in the list!");
+                    }
+                }
+                else
+                {
+                    if (names.Contains(name))
+                    {
+                        Console.WriteLine($"{name} is already in the list!");
+                    }
+                    else
+                    {
+                        names.Add(name);
+                    }
+                }
+            }
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
         }
     }
 }
