7cb3ae26f07dd2ac6e8bbf0dad3680f744025782
MetodyAleksov <71280200+MetodyAleksov@users.noreply.github.com>
2020-10-16 17:13:56 +0300

    Updated

---
+++ 02.ChangeList/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace _02.ChangeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            List<string> command = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            while (command[0] != "end")
+            {
+                if (command[0] == "Delete")
+                {
+                    numbers = Delete(numbers, int.Parse(command[1]));
+                }
+                else if (command[0] == "Insert")
+                {
+                    numbers.Insert(int.Parse(command[2]), int.Parse(command[1]));
+                }
+                command = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+        static List<int> Delete(List<int> list, int n)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (n == list[i])
+                {
+                    list.Remove(n);
+                }
+            }
+            return list;
+        }
+    }
+}
