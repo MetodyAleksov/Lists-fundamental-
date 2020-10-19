ca7bfb90786ccda5b0912d4dcdc235b33fb536dc
MetodyAleksov <71280200+MetodyAleksov@users.noreply.github.com>
2020-10-19 14:48:28 +0300

    updated

---
+++ 09.Pokemon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09.Pokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
