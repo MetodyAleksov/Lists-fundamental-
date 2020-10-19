9b976f81ec496844c7bb4bab2212dd0c09e7d559
MetodyAleksov <71280200+MetodyAleksov@users.noreply.github.com>
2020-10-19 18:30:09 +0300

    Updated

--- 01.Messaging/Program.cs
+++ 01.Messaging/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
@@ -16,32 +17,34 @@ namespace _01.Messaging
 				.Select(int.Parse)
 				.ToList();
 			string sentence = Console.ReadLine();
+			List<char> actualSentence = sentence.ToList();
 			List<char> finalSentence = new List<char>();
-			foreach (int item in numbers)
-			{
-				int n = item;
+            foreach (int item in numbers)
+            {
 				int sum = 0;
+				int n = item;
 				while (n != 0)
 				{
 					sum += n % 10;
 					n /= 10;
 				}
 				int counter = 0;
-				for (int i = 1; i <= sum; i++)
-				{
-					if (counter >= sentence.Length)
-					{
+                for (int i = 0; i < sum; i++)
+                {
+					if (counter == sentence.Length)
+                    {
 						counter = 0;
-					}
-					if (i == sum)
-					{
-						finalSentence.Add(sentence[counter]);
-						sentence.RemoveAt(counter)
-
-					}
-				}
+                    }
+					if (i == sum - 1)
+                    {
+						finalSentence.Add(actualSentence[counter]);
+						actualSentence.RemoveAt(counter);
+                    }
+					counter++;
+                }
+				counter = 0;
 			}
-			Console.WriteLine(string.Join("", finalSentence));
+            Console.WriteLine(string.Join("", finalSentence));
 		}
 	}
 }
---
+++ 02.Cars/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            List<int> car1 = new List<int>();
+            List<int> car2 = new List<int>();
+
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+                car1.Add(arr[i]);
+            }
+            for (int i = arr.Length - 1; i > arr.Length / 2; i--)
+            {
+                car2.Add(arr[i]);
+            }
+            double sumCar1 = 0;
+            foreach (int item in car1)
+            {
+                if (item == 0)
+                {
+                    sumCar1 *= 0.80;
+                }
+                else
+                {
+                    sumCar1 += item;
+                }
+            }
+            double sumCar2 = 0;
+            foreach (int item in car2)
+            {
+                if (item == 0)
+                {
+                    sumCar2 *= 0.80;
+                }
+                else
+                {
+                    sumCar2 += item;
+                }
+            }
+            string winner = String.Empty;
+            double winningTime = 0;
+            if (sumCar1 <= sumCar2)
+            {
+                winner = "left";
+                winningTime = sumCar1;
+            }
+            else
+            {
+                winner = "right";
+                winningTime = sumCar2;
+            }
+            Console.WriteLine($"The winner is {winner} with total time: {winningTime}");
+        }
+    }
+}
